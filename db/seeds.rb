ec8a025e99a6a8e4c00dbddc006ed3ecb72b7177
Mingholy <liminghao15@mails.ucas.ac.cn>
2016-01-16 02:46:01 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create!([
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+{title: '数据挖掘',author: '123', publish_time: '2012-01-01', version:'ee', pro: 12, con: 23, difficulty: 2, clicks: 100, description: 'jdlkajfkljsldfj'},
+])
+
+Course.create!([
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+{title: '软件工程', school: 'ucas', time: '2015-01-01', description: 'adfasfasfasdf',clicks: 123},
+])
